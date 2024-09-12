91e705fd0328774589bccf79c7c7e0aac8977aae
Garret Starke <gstarke@live4biking.com>
2024-09-11 18:57:17 -0700

    added TDD development and tests for palindrome

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ function Phrase(content) {
 
     // Returns content processed for palindrome testing.
     this.processedContent = function processedContent() { 
-        return this.content.toLowerCase();
+        return this.letters().toLowerCase();
+    }
+
+    // Returns the letters in the content.
+    this.letters = function letters() {
+        let theLetters = [];
+        for ( let i = 0 ; i < this.content.length ; i++ ) {
+            if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+                theLetters.push(this.content.charAt(i));
+            }
+        }
+        return theLetters.join("");
     }
 
     // Return true for a palindrome, false otherwise.
