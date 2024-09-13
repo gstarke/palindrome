3fa0baa33060090c14c51e156aa982f597d375d3
Garret Starke <gstarke@live4biking.com>
2024-09-12 19:38:03 -0700

    adding handling of empty string

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ function Phrase(content) {
 
     // Return true for a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent().reverse();
+        if (this.letters()) {
+            return this.processedContent() === this.processedContent().reverse();
+        }
+        else {
+            return false;
+        }
     }
 }
